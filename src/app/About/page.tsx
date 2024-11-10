1373dcef2a22067ac820b46c0fd4c533ef96f0a0
Arisha Ghaffar <160336030+Arishay@users.noreply.github.com>
2024-11-10 12:20:36 +0500

    Add files via upload

---
+++ src/app/About/page.tsx
@@ -0,0 +1,23 @@
+import Summary from "@/static/Summary";
+import Timeline from "@/static/Timeline";
+
+
+function About() {
+    return <div className="bg-slate-800" id="About">
+        <img className="w-full h-44" src="/transitions/transition_gradient.svg" alt="transition" />
+        <div className="flex justify-center items-center text-white">
+            <div className="flex flex-row w-full">
+                <div className="flex desktop:w-1/2 mobile:w-full mobile:p-4 flex-col desktop:p-20">
+                    <Summary />
+                </div>
+                <div className="desktop:flex mobile:hidden w-1/2 flex-col pl-10">
+                    {/* <Timeline /> */}
+                    <img className=" mobile:w-full mobile:max-w-xl object-contain self-center animate-scalepulse" src="/portraitdummy.webp" alt="Portrait" />
+
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default About;
---
+++ src/static/Summary.tsx
@@ -0,0 +1,12 @@
+
+function Summary(){
+    return (<>
+        <p className="text-4xl mb-8">About me</p>
+        <article className="desktop:text-xl mobile:text-md" itemScope itemType="https://schema.org/author">
+            <p className="mb-5">I&apos;m  learning development in Giaic i have some knowledge of website development through wordpress right now i am in 10th class 🎓.
+                My main focus these days is to learn nextjs and react to build high performance web applications for desktop and mobile devices using the latest technologies. </p>
+        </article>
+    </>);
+}
+
+export default Summary;
